770cee3ddb951f23ae00343d289c8e5fe6ae7532
Juan Bermudez <juan.bermudez1102@outlook.com>
2019-09-22 02:35:23 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
   counter = 0;
-  while i < array.length do
+  while counter < array.length do
     array[counter] = array[counter]**2
-    i += 1
+    counter += 1
   end
 end
